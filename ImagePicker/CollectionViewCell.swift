64bc10a7b5e7710eadf38a98e566a2d5c070e62c
Dzmitry Safin <Dzmitry@192.168.100.10>
2016-01-30 19:52:16 +0300

    UI for using image picker

---
+++ ImagePicker/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  ImagePicker
+//
+//  Created by Dzmitry Safin on 1/30/16.
+//  Copyright © 2016 Dzmitry Safin. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+}
